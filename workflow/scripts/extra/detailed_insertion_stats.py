c818965fe59f9d8b6eeb1dc34b864a3d12e50129
AntonJMLarsson <anton.larsson@ki.se>
2025-05-29 15:11:20 +0200

    some more code

--- workflow/scripts/extra/detailed_insertion_stats.py
+++ workflow/scripts/extra/detailed_insertion_stats.py
@@ -18,6 +18,33 @@ def get_tag_site(read):
         return read.reference_end
     else:
         return read.reference_start
+    
+def count_tag_sites_and_read_depth(bamfile, bamfile_polyA, cell_BC_set, idx, row, tag):
+    tag_sites_per_cell_counter = {}
+    
+    bam = pysam.AlignmentFile(bamfile, 'rb')
+    for read in bam.fetch(row['chrom'], row['start'], row['end']):
+        cell = read.get_tag(tag)
+        if cell not in tag_sites_per_cell_counter and cell in cell_BC_set:
+            tag_sites_per_cell_counter[cell] = Counter()
+        if read.mapping_quality > 0 and not read.is_supplementary and not read_is_softclip(read):
+            tag_sites_per_cell_counter[cell].update({get_tag_site(read): 1})
+    bam.close()
+
+    bam = pysam.AlignmentFile(bamfile_polyA, 'rb')
+    for read in bam.fetch(row['chrom'], row['start'], row['end']):
+        cell = read.get_tag(tag)
+        if cell not in tag_sites_per_cell_counter and cell in cell_BC_set:
+            tag_sites_per_cell_counter[cell] = Counter()
+        if read.mapping_quality > 0 and not read.is_supplementary and not read_is_softclip(read):
+            tag_sites_per_cell_counter[cell].update({get_tag_site(read): 1})
+    bam.close()
+
+    res = []
+    for cell, cell_tag_site_counter in tag_sites_per_cell_counter.items():
+        for tag_site, count in cell_tag_site_counter.items():
+            res.append([cell, idx, tag_site, count])
+    return res
 
 def main():
     parser = argparse.ArgumentParser(description='Count cell detection', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -25,6 +52,7 @@ def main():
     parser.add_argument('-bam-polyA', '--bam_input_polyA',  metavar='polyA_input', type='str', help='Remapped .bam file')
     parser.add_argument('-stats','--stats', metavar='stats', type=str, help='Input stats file')
     parser.add_argument('-sample','--samplesheet', metavar='samplesheet', type=str, help='Samplesheet file')
+    parser.add_argument('-d','--donor', metavar='donor', type=str, help='Donor')
     parser.add_argument('-p','--prefix', metavar='prefix', type=str, help='prefix')
     parser.add_argument('-ct','--tag', metavar='tag', type=str, help='cell tag')
     parser.add_argument('-t','--threads', metavar='threads', type=int, help='threads')
@@ -38,11 +66,11 @@ def main():
     tag = args.tag
     threads = args.threads
     samplesheet = pd.read_csv(samplesheet_file, index_col=0)
-    cell_BC_set = set(samplesheet.index)
+    cell_BC_set = set(samplesheet[samplesheet['Donor'] == args.donor].index)
 
     stats_df = pd.read_csv(stats_file, index_col=0)
 
-    res = Parallel(n_jobs=threads, verbose = 3, backend='loky')(delayed(count_tag_sites_and_read_depth)(bamfile, cell_BC_set, idx, row, tag) for idx, row in stats_df.iterrows())
+    res = Parallel(n_jobs=threads, verbose = 3, backend='loky')(delayed(count_tag_sites_and_read_depth)(bamfile, bamfile_polyA, cell_BC_set, idx, row, tag) for idx, row in stats_df.iterrows())
     cell_counter_dict_total = {idx: cell_counter_dict for idx, cell_counter_dict in res}
     read_depth_df, tag_site_df = make_tables(cell_counter_dict_total,cell_BC_set)
     make_stats_per_donor(samplesheet, stats_df, read_depth_df, tag_site_df, prefix)
